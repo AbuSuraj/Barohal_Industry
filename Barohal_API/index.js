5bbd3722401cdee56ec224fbc9032a88380dfb7a
Abu Suraj <abusurajbd@gmail.com>
2023-06-16 00:34:08 +0600

    updated

--- Barohal_API/index.js
+++ Barohal_API/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { resetWatchers } = require("nodemon/lib/monitor/watch");
+// const { resetWatchers } = require("nodemon/lib/monitor/watch");
 const app = express();
 const port = process.env.PORT || 5000;
 
